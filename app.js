ce932a4c74fad51fea227d35ac7df86019ed95c0
waltonbmw <brettwalton27@gmail.com>
2016-04-09 17:42:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6Lcm2xwTAAAAADh3Q1lSbxJg3c-4LzU2ESJENJyT',  // <----- EDIT ME!
-  redirect_uri: 'https://www.moneypot.com/oauth/authorize?app_id=1065&redirect_uri=https://illevate.github.io',
+  redirect_uri: 'https://illevate.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
